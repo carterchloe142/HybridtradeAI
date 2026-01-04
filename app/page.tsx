6b01ed2c38130771379078bdfcff82dfccb78ac2
othesisrentals <othesisrentals@gmail.com>
2026-01-05 00:11:54 +0100

    Disable Pages Router by renaming to pages-old to fix 404

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-static";
+
 export default function Home() {
   return (
     <main style={{ minHeight: "100vh", background: "#0b1020", color: "white", padding: "40px" }}>
