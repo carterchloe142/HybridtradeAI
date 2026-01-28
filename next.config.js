1c37bddee2e4bea0764757878b738a54b75f6880
othesisrentals <othesisrentals@gmail.com>
2026-01-28 09:59:49 +0100

    Deployment fix: Disable source maps and add logging for cache disable

--- next.config.js
+++ next.config.js
@@ -10,8 +10,10 @@ const nextConfig = {
   output: 'standalone',
   reactStrictMode: true,
   images: { unoptimized: true },
+  productionBrowserSourceMaps: false,
   webpack: (config, { dev }) => {
     if (!dev) {
+      console.log('--- Disabling webpack cache for production build ---');
       config.cache = false;
     }
     return config;
